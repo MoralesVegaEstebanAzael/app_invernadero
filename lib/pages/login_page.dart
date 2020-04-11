90f9462d46c85ff244658106fc0824c2e9976b2a
Azaemorales2329 <azaelmorales029@gmail.com>
2020-04-11 13:05:26 -0500

    Initial commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+
+class LoginPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 400,
+            child: Stack(
+              children:<Widget>[
+                Positioned(
+                  child:Container(
+                    decoration: BoxDecoration(backgroundBlendMode: )
+                  ) ,)
+              ]
+            ),
+          )
+        ]
+      ),
+    );
+  }
+}
